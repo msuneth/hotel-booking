117a710e9cec5ec496ad2c1ec06bc6a135682922
msuneth <msuneth@gmail.com>
2024-01-24 06:22:55 +0530

    hotel class methods added

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-df = pd.read_csv("hotels.csv")
+df = pd.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+        print("hotel booked")
 
     def available(self):
-        return True
-
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            print("hotel available")
+            return True
+        else:
+            return False
 
 class ReservationTicket:
     def __init__(self, name, hotel):
@@ -23,8 +29,8 @@ class ReservationTicket:
 
 
 print(df)
-id = input("Enter hotel Id:")
-hotel = Hotel(id)
+hotel_Id = input("Enter hotel Id:")
+hotel = Hotel(hotel_Id)
 
 if hotel.available():
     hotel.book()
